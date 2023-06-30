fb85713c2836577e144f2de29b7ff2229eaaf704
Yamen Mardini <46592887+Y-Mardini@users.noreply.github.com>
2023-06-30 12:26:15 +0200

    Update Cinema.kt

--- Cinema.kt
+++ Cinema.kt
@@ -1,19 +1,66 @@
 package cinema
+var rowsCount: Int = 0
+var seatsInRow: Int = 0
 
-fun main() {
+fun createCinemaMatrix ():MutableList<MutableList<String>>
+{
+
+    val cinemaIndicatorLine = MutableList(seatsInRow+1) {" "}
+    val cinemaMatrix = mutableListOf<MutableList<String>>()
+
+    for (i in 1..seatsInRow) { cinemaIndicatorLine[i]=i.toString();}
+    cinemaMatrix.add(cinemaIndicatorLine)
+
+    for(i in 1 .. rowsCount)
+    {
+        val rowOfSeats = MutableList(seatsInRow){"S"}
+        rowOfSeats.add(0,i.toString())
+        cinemaMatrix.add(rowOfSeats)
+    }
+
+    return cinemaMatrix
+}
+
+fun printCinemaMatrix (cinemaMatrix: MutableList<MutableList<String>>)
+{
     println("Cinema:")
-    //print the horizontal index
-    println("  1 2 3 4 5 6 7 8")
-    val seatsCount: Int = 8
-    val rowCount: Int = 7
-    var seatsGraphic: String =""
+    for (i in cinemaMatrix.indices)
+    {
+        for (j in cinemaMatrix[i].indices)
+        {
+            print(" "+cinemaMatrix[i][j])
+        }
+       print("\n")
+    }
 
-    //Create the seat line
-    repeat(seatsCount) {
-        seatsGraphic += "S "
-    }
-    //Layout the Cinema hall
-    repeat(rowCount) {
-        println("${it + 1} $seatsGraphic" )
-    }
+}
+
+fun bookSeat (cinemaMatrix: MutableList<MutableList<String>>):MutableList<MutableList<String>>
+{
+    println("Enter a row number:")
+    val rowNumber = readln().toInt()
+    println("Enter a seat number in that row:")
+    val seatNumber = readln().toInt()
+
+    val ticketPrice = if (rowsCount * seatsInRow  >= 60 && rowNumber > rowsCount/2) { 8 } else  { 10 }
+
+    println("Ticket price: \$"+ticketPrice)
+
+    cinemaMatrix[rowNumber][seatNumber]="B"
+
+    return cinemaMatrix
+}
+fun main() {
+
+    println("Enter the number of rows:")
+    rowsCount = readln().toInt()
+    println("Enter the number of seats in each row:")
+    seatsInRow = readln().toInt()
+
+    var cinemaMatrix: MutableList<MutableList<String>> = createCinemaMatrix()
+    printCinemaMatrix(cinemaMatrix)
+
+    cinemaMatrix = bookSeat(cinemaMatrix)
+
+    printCinemaMatrix(cinemaMatrix)
 }
