0d26cec6a82b0b90931d884564d50b38b234292d
Yamen Mardini <46592887+Y-Mardini@users.noreply.github.com>
2023-07-05 10:39:56 +0200

    Update Cinema.kt
    Add menu

--- Cinema.kt
+++ Cinema.kt
@@ -23,7 +23,7 @@ fun createCinemaMatrix ():MutableList<MutableList<String>>
 
 fun printCinemaMatrix (cinemaMatrix: MutableList<MutableList<String>>)
 {
-    println("Cinema:")
+    println("\nCinema:")
     for (i in cinemaMatrix.indices)
     {
         for (j in cinemaMatrix[i].indices)
@@ -35,11 +35,12 @@ fun printCinemaMatrix (cinemaMatrix: MutableList<MutableList<String>>)
 
 }
 
+
 fun bookSeat (cinemaMatrix: MutableList<MutableList<String>>):MutableList<MutableList<String>>
 {
-    println("Enter a row number:")
+    println("\nEnter a row number:")
     val rowNumber = readln().toInt()
-    println("Enter a seat number in that row:")
+    println("\nEnter a seat number in that row:")
     val seatNumber = readln().toInt()
 
     val ticketPrice = if (rowsCount * seatsInRow  >= 60 && rowNumber > rowsCount/2) { 8 } else  { 10 }
@@ -50,6 +51,7 @@ fun bookSeat (cinemaMatrix: MutableList<MutableList<String>>):MutableList<Mutabl
 
     return cinemaMatrix
 }
+
 fun main() {
 
     println("Enter the number of rows:")
@@ -58,9 +60,15 @@ fun main() {
     seatsInRow = readln().toInt()
 
     var cinemaMatrix: MutableList<MutableList<String>> = createCinemaMatrix()
-    printCinemaMatrix(cinemaMatrix)
 
-    cinemaMatrix = bookSeat(cinemaMatrix)
+    while (true)
+    {   println("\n1. Show the seats\n2. Buy a ticket \n0. Exit")
+        when (readln().toInt())
+        {
+            1 -> printCinemaMatrix(cinemaMatrix)
+            2 -> cinemaMatrix = bookSeat(cinemaMatrix)
+            else -> break
+        }
+    }
 
-    printCinemaMatrix(cinemaMatrix)
 }
